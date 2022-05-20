ffbca0187b2cc29fb0feef331ba31012de309cd8
unknown <kweyz@daum.net>
2022-05-20 21:41:23 +0900

    fix valid

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -3,10 +3,11 @@ const loginId = document.querySelector("#loginId");
 const loginPassword = document.querySelector("#loginPassword");
 
 const validate = () => {
-  if (loginId.value == "") {
+  if (loginId.value == "" && loginPassword.value == "") {
+    alert("아이디, 패스워드를 입력하세요.");
+  } else if (loginId.value == "") {
     alert("아이디를 입력하세요.");
-  }
-  if (loginPassword.value == "") {
+  } else if (loginPassword.value == "") {
     alert("패스워드를 입력하세요.");
   }
 };
