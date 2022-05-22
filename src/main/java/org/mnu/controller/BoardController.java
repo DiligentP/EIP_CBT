fdc839642bf347457a94a43e5027fce63032e7ab
KappyDays <rkdfuf2wkd@naver.com>
2022-05-22 22:31:06 +0900

    BoardController 작성

---
+++ src/main/java/org/mnu/controller/BoardController.java
@@ -0,0 +1,26 @@
+package org.mnu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.mnu.controller.BoardController;
+import org.mnu.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
+}
