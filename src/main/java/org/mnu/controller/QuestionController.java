99a73247270821457756be2652f362771d3e2b9b
DiligentP <qkrwjdgus0603@gmail.com>
2022-05-28 22:31:38 +0900

    Add QuestionController.java

---
+++ src/main/java/org/mnu/controller/QuestionController.java
@@ -0,0 +1,87 @@
+package org.mnu.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Log4j
+@AllArgsConstructor
+@RequestMapping("/question/*")
+public class QuestionController {
+    /**
+     * @brief 문제 리스트 페이지
+     * @details 문제 리스트 페이지
+     */
+    @GetMapping("/list")
+    public void list() {
+
+    }
+
+    /**
+     * @brief 문제 수정 페이지
+     * @details 문제의 정보를 수정할 수 있는 페이지를 보여준다.
+     */
+    @GetMapping("/modify")
+    public void modify() {
+
+    }
+
+    /**
+     * @brief 문제 수정 처리 페이지
+     * @details 입력한 정보를 DB에 반영하여 정보를 수정한다.
+     */
+    @PostMapping("/modify")
+    public String modify(RedirectAttributes rttr) {
+        return "redirect:/question/list";
+    }
+
+    /**
+     * @brief 문제 삭제 처리 페이지
+     * @details 문제를 삭제하고 문제 리스트 페이지로 리다이렉트한다.
+     */
+    @GetMapping("/remove")
+    public String remove(RedirectAttributes rttr) {
+        return "redirect:/question/list";
+    }
+
+    /**
+     * @brief 시험 시작 설정 페이지
+     * @details 시험에 필요한 문제를 선택하는 페이지
+     */
+    @GetMapping("/exam/config")
+    public void examConfig() {
+
+    }
+
+    /**
+     * @brief 시험 페이지
+     * @details 문제를 골라서 시험을 보는 페이지
+     */
+    @GetMapping("/exam/progress")
+    public void examProgress() {
+
+    }
+
+    /**
+     * @brief 채점 처리
+     * @details 채점 처리
+     */
+    @PostMapping("/exam/grading")
+    public void examGrading() {
+
+    }
+
+    /**
+     * @brief 시험 결과 페이지
+     * @details 시험 결과 페이지
+     */
+    @GetMapping("/exam/result")
+    public void examResult() {
+
+    }
+}
