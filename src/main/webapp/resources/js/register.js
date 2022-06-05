af30c4a26b0e6e3efdd78e1df5b8e22a021188b8
unknown <kweyz@daum.net>
2022-06-06 00:27:26 +0900

    valid register fix

--- src/main/webapp/resources/js/register.js
+++ src/main/webapp/resources/js/register.js
@@ -4,6 +4,8 @@ const registerId = document.querySelector("#registerId");
 const registerPassword = document.querySelector("#registerPassword");
 const registerEmail = document.querySelector("#registerEmail");
 
+let Check = false;
+
 // 유효성 검사 함수
 const validate = () => {
   // 이름 정규화
@@ -21,27 +23,39 @@ const validate = () => {
     registerId.value == ""
   ) {
     alert("빈칸을 모두 채워주세요.");
+    return (Check = false);
   } else if (registerId.value == "") {
     alert("아이디를 입력해주세요.");
+    return (Check = false);
   } else if (!regId.test(registerId.value)) {
     alert("아이디는 6~20이하 대소문자 또는 숫자 포함해야합니다.");
+    return (Check = false);
   } else if (registerPassword.value == "") {
     alert("패스워드를 입력해주세요.");
+    return (Check = false);
   } else if (!regPw.test(registerPassword.value)) {
     alert("비밀번호는 8~32이하 대소문자, 숫자, 특수문자 포함해야합니다.");
+    return (Check = false);
   } else if (registerName.value == "") {
     alert("이름을 입력해주세요.");
+    return (Check = false);
   } else if (!regName.test(registerName.value)) {
     alert("이름을 잘못 입력하였습니다.");
+    return (Check = false);
   } else if (registerEmail.value == "") {
     alert("이메일을 입력해주세요.");
+    return (Check = false);
   } else {
     alert("✨축하합니다! 로그인 해주세요.");
+    return (Check = true);
   }
 };
 
-const handleSubmit = () => {
+const handleSubmit = (e) => {
   validate();
+  if (Check === false) {
+    e.preventDefault();
+  }
 };
 
 registerSubmit.addEventListener("click", handleSubmit);
